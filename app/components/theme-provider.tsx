4f981021d6ee36549eaa67997293dc386998816f
suvanl <32813609+suvanl@users.noreply.github.com>
2024-11-09 23:08:26 +0000

    Delete theme-provider.tsx

--- app/components/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  // Despite the name, next-themes is now React framework agnostic
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
