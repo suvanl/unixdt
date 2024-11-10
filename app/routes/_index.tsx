1270bb16aecc0293b84de006ecb3b309dc12cb21
suvanl <32813609+suvanl@users.noreply.github.com>
2024-11-10 10:34:24 +0000

    Add grid layout on larger screens

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -21,24 +21,39 @@ export default function Index() {
     <>
       <TopAppBar />
 
-      <main className="container my-16">
-        <div className="flex items-center gap-2">
-          <h2>Current Unix timestamp</h2>
-          <Tooltip>
-            <TooltipTrigger>
-              <HelpCircleIcon className="text-muted-foreground size-4" />
-              <span className="sr-only">Help</span>
-            </TooltipTrigger>
-            <TooltipContent className="w-[300px]">
-              <p className="text-xs">
-                Number of seconds since the Unix epoch (1st January 1970 00:00
-                UTC)
-              </p>
-            </TooltipContent>
-          </Tooltip>
-        </div>
+      <main className="container my-16 space-y-20">
+        <section>
+          <div className="flex items-center gap-2">
+            <h2>Current Unix timestamp</h2>
+            <Tooltip>
+              <TooltipTrigger>
+                <HelpCircleIcon className="size-4 text-muted-foreground" />
+                <span className="sr-only">Help</span>
+              </TooltipTrigger>
+              <TooltipContent className="w-[300px]">
+                <p className="text-xs">
+                  Number of seconds since the Unix epoch (1st January 1970 00:00
+                  UTC)
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
 
-        <UnixClock />
+          <UnixClock />
+        </section>
+
+        <section className="grid gap-16 lg:grid-cols-2">
+          <section>
+            <p className="text-pretty">
+              Enter a Unix timestamp (in seconds or milliseconds) to convert to
+              a human-readable date
+            </p>
+          </section>
+
+          <section>
+            <p>Select a time and date to convert to a Unix timestamp</p>
+          </section>
+        </section>
       </main>
 
       <Footer />
