70b463ccd6c4b25334f3bee7fdab781ef9fa5c5d
suvanl <32813609+suvanl@users.noreply.github.com>
2026-02-20 23:16:54 +0000

    delete all

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  semi: true,
-  singleQuote: false,
-  quoteProps: "consistent",
-};
-
-export default config;
